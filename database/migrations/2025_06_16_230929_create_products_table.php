51f698f64b57cf65ecad02a59f80645de7d80f5d
Lucas Alves <142865401+LucasAlv021@users.noreply.github.com>
2025-06-16 21:16:13 -0300

    resolvido o problema no produtos

--- database/migrations/2025_06_16_230929_create_products_table.php
+++ database/migrations/2025_06_16_230929_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('esporte');
+            $table->string('preco');
+            $table->string('imagem')->nullable();
             $table->timestamps();
         });
     }
